b8a0eadfd3627a96f1d0eaa57502e429101fcd47
guigonzalez <guidogermang@gmail.com>
2022-06-16 18:51:11 -0300

    troubleshoot lower

--- cronos.py
+++ cronos.py
@@ -199,7 +199,6 @@ def send_message(response, update,context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
 dispatcher.add_handler(CommandHandler("start", startCommand))
-dispatcher.add_handler(CommandHandler("Dia", diaCommand))
 dispatcher.add_handler(CommandHandler("día", diaCommand))
 dispatcher.add_handler(CommandHandler("Entrada", entradaCommand))
 dispatcher.add_handler(CommandHandler("Comida", comidaCommand))
